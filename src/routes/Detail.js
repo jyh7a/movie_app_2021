acd37a52b27731db8cff13f70844e965bb8fd9bc
YoungHun Jeong <jyh7a@naver.com>
2021-08-25 01:21:46 +0900

    최종 완성 코드

---
+++ src/routes/Detail.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import "./Detail.css";
+
+class Detail extends Component {
+  componentDidMount() {
+    const { location, history } = this.props;
+    if (location.state === undefined) {
+      history.push("/");
+    }
+    // console.log("location", location);
+    // console.log("history", history);
+  }
+
+  render() {
+    if (!this.props.location.state) return null;
+
+    const {
+      location: {
+        state: { title, year, summary, poster, genres },
+      },
+    } = this.props;
+
+    return (
+      <div className="detail">
+        <div className="detail__container">
+          <img className="detail__poster" src={poster} alt={title} />
+
+          <div className="detail__data">
+            <h1 className="detail__title">{title}</h1>
+            <div className="detail__year">{year}</div>
+            <ul className="detail__genres">
+              {genres.map((genre, index) => {
+                return <li key={index}>{genre}</li>;
+              })}
+            </ul>
+            <div className="detail__summary">{summary}</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Detail;
